a73c630300327671b6efd0a7e5b282354292e1c0
HasanC14 <chowdhuryh999@gmail.com>
2023-03-20 10:48:30 +0600

    some functionality added

--- app/src/main/java/com/hasanchodhury/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/hasanchodhury/bmicalculator/MainActivity.kt
@@ -2,10 +2,25 @@ package com.hasanchodhury.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<TextView>(R.id.btn)
+
+        btn.setOnClickListener {
+            val result = findViewById<TextView>(R.id.result)
+            val age = findViewById<TextView>(R.id.age).text.toString()
+            val male = findViewById<TextView>(R.id.male)
+            val female = findViewById<TextView>(R.id.female)
+            val height = findViewById<TextView>(R.id.height).text.toString()
+            val weight = findViewById<TextView>(R.id.weight).text.toString()
+
+            result.text = ("Age- $age, Height- $height, Weight- $weight")
+        }
     }
 }
